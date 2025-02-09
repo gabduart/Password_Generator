495e8907d5ea92727701bb2630f725c44c485118
gabduart <gabduart68@gmail.com>
2025-02-09 20:10:11 -0300

    generate_password() function added and testing

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello python!")
+import string
+import random
+
+def generate_password(length=16):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
+
+if __name__ == "__main__":
+    print("Generated password:", generate_password())
