1ae879d75ead12636282201af03bcdc884697376
gabduart <gabduart68@gmail.com>
2025-02-09 20:19:36 -0300

    encrypt_password() implemented and tested

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import string
 import random
+import hashlib
 
 def generate_password(length=16):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
     return password
 
+def encrypt_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
 if __name__ == "__main__":
-    print("Generated password:", generate_password())
+    password = generate_password()
+    print("Generated password:", password)
+    print("Encrypted password:", encrypt_password(password))
